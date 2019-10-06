39a6b90afa0d9e9336d347a7061e42b88c202d40
josehmaza <josehmaza@gmail.com>
2019-10-05 21:23:32 -0500

    Clase 21: El contexto de las funcinoes: quies es this

--- clase20.js
+++ clase20.js
@@ -1,7 +1,8 @@
 // CLase 20: COmo funcionan las clases en javascript
-const Persona = function(nombre, apellido){
+const Persona = function(nombre, apellido, altura){
     this.nombre = nombre
     this.apellido = apellido
+    this.altura = 1.82
     
 }
 // Esto no puedo ir dentro de la declaracion del functino Persona
@@ -9,6 +10,19 @@ const Persona = function(nombre, apellido){
 Persona.prototype.saludar = function () {
     console.log(`Hola soy ${this.nombre} ${this.apellido}`)
 }
-let hernan = new Persona('Hernan', 'Maza')
+Persona.prototype.soyAlto = function()  {
+    return this.altura > 1.8
+}
+
+//Clase 21: El contexto de las funciones: quién es this
+//Cuando uso arrow functions, esto reemplaza el this x el this externo, es decir el this(dentro del arrow function) = this fuera del arrow functinos
+//Persona.prototype.soyAlto = () => {
+//    return this.altura > 1.8
+//}
+// this // Esto es lo que se reemplazara el contexto this dentro del arrow function
+
+// Se debe aplicar la palabra new
+let hernan = new Persona('Hernan', 'Maza', 1.85)
 
 hernan.saludar()
+console.log(hernan.soyAlto())
