17ee3fa8b8905760763ef0963a372c7d029feab0
josehmaza <josehmaza@gmail.com>
2019-10-05 20:19:49 -0500

    Clase 19: Transformar un array

--- clase17.js
+++ clase17.js
@@ -41,6 +41,12 @@ for(var i =0 ; i < personas.length; i++){
     console.log(`${persona.nombre} tiene ${persona.altura} de altura`)
 }
 //Clase 18
+// 1- Filtrar un array requiere de dos cosas, un array para guardar los datos filtrados y una función que permita aplicar el filtro a los datos
+
+// 3- Si los datos cumplen con el filtro, el método filter de manera implicita crea una copia de dicho elemento declarado en el arreglo original y lo empluja a la variable asignada...
+
+// Filter recibe una función callback, que es invocada tantas veces como elementos haya dentro del arreglo
+
 const esAlta = ({altura}) => altura > 1.8
 let personasAltas = personas.filter(esAlta)
 console.log(personasAltas)
@@ -48,3 +54,16 @@ console.log(personasAltas)
 
 let personasBajas = personas.filter(persona => !esAlta(persona))
 console.log(personasBajas)
+
+// Si quiero retornar un objeto desde la arrow function debo ponerle entre parentesis persona => ({})
+const pasarAlturaAcms = persona => ({
+        ...persona,
+        altura: persona.altura *100
+})
+// Clase 19: Transformar un array
+/* En esta clase veremos cómo transformar un array. 
+El método map() itera sobre los elementos de un array en el orden de inserción y 
+devuelve array nuevo con los elementos modificados.*/
+console.log('Personas con altura en cms')
+let personasAlturacms = personas.map(pasarAlturaAcms)
+console.log(personasAlturacms)
